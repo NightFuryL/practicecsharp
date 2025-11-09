4b05b6c2122b96057135469d9ecce2351288b7ab
NightFuryL <levodesskiy@gmail.com>
2025-11-09 13:39:13 +0200

    Add 5 - 11 exercises to pratcice 09.11.2025

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 //Трохи Math додав для полегшення деяких обчислень
+//Трохи складненько було, а ось так
 class Program
 {
     static void Main()
@@ -17,9 +18,9 @@ class Program
         for (int i = 0; i < arr1.Length; i++)
         {
             bool found = false;
-            for (int j = 0; j < arr1.Length; j++)
+            for (int jj = 0; jj < arr1.Length; jj++)
             {
-                if (i != j && arr1[i] == arr1[j])
+                if (i != jj && arr1[i] == arr1[jj])
                 {
                     found = true;
                     break;
@@ -62,14 +63,18 @@ class Program
         int[] C = new int[Math.Min(A.Length, B.Length)];
         int k = 0;
         for (int i = 0; i < A.Length; i++)
-            for (int j = 0; j < B.Length; j++)
-                if (A[i] == B[j])
+        {
+            for (int jj = 0; jj < B.Length; jj++)
+            {
+                if (A[i] == B[jj])
                 {
                     bool exists = false;
                     for (int t = 0; t < k; t++)
                         if (C[t] == A[i]) { exists = true; break; }
                     if (!exists) C[k++] = A[i];
                 }
+            }
+        }
 
         for (int i = 0; i < k; i++)
             Console.Write(C[i] + " ");
@@ -82,12 +87,136 @@ class Program
         int min = arr[0, 0];
         int max = arr[0, 0];
         for (int i = 0; i < arr.GetLength(0); i++)
-            for (int j = 0; j < arr.GetLength(1); j++)
+            for (int jjj = 0; jjj < arr.GetLength(1); jjj++)
             {
-                if (arr[i, j] < min) min = arr[i, j];
-                if (arr[i, j] > max) max = arr[i, j];
+                if (arr[i, jjj] < min) min = arr[i, jjj];
+                if (arr[i, jjj] > max) max = arr[i, jjj];
             }
         Console.WriteLine("Min : " + min);
         Console.WriteLine("Max : " + max);
+        //6
+        Console.Write("Sentence: ");
+        string sentence = Console.ReadLine();
+        string[] words = sentence.Split(' ');
+        int wordCount = 0;
+        for (int i = 0; i < words.Length; i++)
+            if (words[i] != "") wordCount++;
+        Console.WriteLine("Count of words: " + wordCount);
+        Console.WriteLine();
+        //7
+        Console.Write("Sentence: ");
+        string s = Console.ReadLine();
+        string[] parts = s.Split(' ');
+        for (int i = 0; i < parts.Length; i++)
+        {
+            char[] arr4 = parts[i].ToCharArray();
+            for (int jjjj = 0; jjjj < arr4.Length / 2; jjjj++)
+            {
+                char temp = arr4[jjjj];
+                arr4[jjjj] = arr4[arr4.Length - 1 - jjjj];
+                arr4[arr4.Length - 1 - jjjj] = temp;
+            }
+            parts[i] = new string(arr4);
+        }
+        Console.WriteLine(string.Join(" ", parts));
+        Console.WriteLine();
+
+        //8
+        Console.Write("Sentence: ");
+        string text = Console.ReadLine().ToLower();
+        string vowels = "aeiou";
+        int count = 0;
+        foreach (char cc in text)
+            foreach (char ccc in vowels)
+                if (cc == ccc)
+                    count++;
+        Console.WriteLine("Count of vowels: " + count);
+        Console.WriteLine();
+        //9
+        Console.Write("Enter a string: ");
+        string mainText = Console.ReadLine();
+        Console.Write("Enter a substring to search: ");
+        string sub = Console.ReadLine();
+        int index = 0, total = 0;
+        while (true)
+        {
+            int found = mainText.IndexOf(sub, index);
+            if (found == -1) break;
+            total++;
+            index = found + sub.Length;
+        }
+        Console.WriteLine("Count of occurrences: " + total);
+        Console.WriteLine();
+
+        //10
+        string str = Console.ReadLine();
+        int n = str.Length;
+        int[] indexes = new int[n];
+        for (int i = 0; i < n; i++) indexes[i] = 0;
+
+        Console.WriteLine(str);
+
+        int j = 0;
+        while (j < n)
+        {
+            if (indexes[j] < j)
+            {
+                char[] chars = str.ToCharArray();
+                int swapIndex = j % 2 == 0 ? 0 : indexes[j];
+                char temp = chars[swapIndex];
+                chars[swapIndex] = chars[j];
+                chars[j] = temp;
+                str = new string(chars);
+                Console.WriteLine(str);
+                indexes[j]++;
+                j = 0;
+            }
+            else
+            {
+                indexes[j] = 0;
+                j++;
+            }
+        }
+        Console.WriteLine();
+        //11
+        string textt = Console.ReadLine();
+        int[] indexess = new int[256];
+        char[] charss = new char[256];
+        for (int i = 0; i < textt.Length; i++)
+        {
+            char currentChar = textt[i];
+            if (currentChar == ' ') continue;
+
+            bool found = false;
+            for (int jj = 0; jj < 256; jj++)
+            {
+                if (charss[jj] == currentChar)
+                {
+                    indexess[jj]++;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                for (int jj = 0; jj < 256; jj++)
+                {
+                    if (charss[jj] == '\0')
+                    {
+                        charss[jj] = currentChar;
+                        indexess[jj] = 1;
+                        break;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 256; i++)
+        {
+            if (charss[i] != '\0')
+            {
+                Console.WriteLine($"{charss[i]}: {indexess[i]}");
+            }
+        }
+
     }
 }
